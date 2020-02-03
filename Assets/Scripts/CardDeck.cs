a26ab97b857750e5cca1a2e6121a538f4ac69fa6
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2020-02-03 13:51:50 +0200

    Создал класс колоды

---
+++ Assets/Scripts/CardDeck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDeck : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    private List<Card> _cards = new List<Card>();
+
+    public event Action<Card> CardSelected;
+
+    private void Start()
+    {
+        foreach (var card in gameObject.GetComponentsInChildren<Card>())
+        {
+            card.Init(_player);
+            card.Selected += OnCardSelected;
+            _cards.Add(card);
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var card in _cards)
+        {
+            card.Selected -= OnCardSelected;
+        }
+    }
+
+    private void OnCardSelected(Card card)
+    {
+        CardSelected?.Invoke(card);
+    }
+}
